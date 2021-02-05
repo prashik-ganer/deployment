3c4002ceefef8889ad19c43d3b9755710a197a4e
Prashik <prashik.ganer@gmail.com>
2021-02-05 12:59:06 +0530

    commented sqlite default database locally in settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -101,12 +101,12 @@ WSGI_APPLICATION = 'mac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 import dj_database_url
